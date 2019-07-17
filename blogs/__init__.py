0be6163262269af1eb1508b77683f6d83ee467e3
xiaotudoushizhenjun <15634118755@163.com>
2019-07-17 16:26:43 +0800

    完成admin的定制，下一步开始开发面向用户的界面

--- blogs/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-'''
-@Author  : xiaotudou
-@time: 2019/07/17 8:27
-@File    : __init__.py.py 
-'''
--- blogs/blogs/settings/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-'''
-@Author  : xiaotudou
-@time: 2019/07/17 8:26
-@File    : __init__.py.py 
-'''
-import pymysql
-pymysql.install_as_MySQLdb()
